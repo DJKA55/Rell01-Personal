03aab6f9013ba73c642234929f6b643d11341694
DJKA55 <166100768+DJKA55@users.noreply.github.com>
2024-04-05 13:25:14 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GreengGarden</title>
+	<link rel="stylesheet" type="text/css" href="aset/css/style.css">
+	<link rel="stylesheet" type="text/css" href="aset/fontawesome/css/all.css">
+</head>
+<body>
+	<div class="wrap">
+		<div class="header">
+			<div class="menu">
+				<img src="aset/img/GG-w200.png">
+				<ul>
+					<li>MENU</li>
+					<li>ABOUT</li>
+					<li>SERVICE</li>
+					<li>GALLERY</li>
+					<li>BLOG</li>
+					<li>CONTACT</li>
+				</ul>
+			</div>
+			<h1>Green View For Good Live</h1>
+			<h4>Prepare Your Garden With Our Professional</h4>
+			<div>
+				<a href="#" class="btn-green">
+					Hire Our Profesional New
+					<i class="fa fa-arrow-right"></i>
+				</a>
+			</div>
+		</div>
+
+			<div class="content-atas">
+				<h2>Why Should We?</h2>
+				<h4>Because we believe you are boss and we serve you with passion</h4>
+
+				<div class="col1">
+
+				<div class="colom1">
+					<i class="fa-4x far fa-star"></i>
+					<h5>Good Reputation Since'89</h5>
+					<p>Lorem ipsum dolor sit amet</p>
+				</div>
+
+				<div class="colom2">
+					<i class="fa-4x fas fa-balance-scale"></i>
+					<h5>Legal Company</h5>
+					<p>Lorem ipsum dolor sit amet</p>
+				</div>
+
+				<div class="colom3">
+					<i class="fa-4x fas fa-award"></i>
+					<h5>Focus On Quality</h5>
+					<p>Lorem ipsum dolor sit amet</p>
+				</div>
+
+				<div class="colom4">
+					<i class="fa-4x fas fa-hand-holding-usd"></i>
+					<h5>Good Price</h5>
+					<p>Lorem ipsum dolor sit amet</p>
+				</div>
+
+				</div>
+			</div>
+
+			<div class="content-bawah">
+			<h2>Our Special Service</h2>
+			<h4>To give you diferent think about garden, makes you feel comfortable</h4>
+
+				<div class="col2">
+
+					<div class="colom-1">
+					<img width="200" height="130" src="aset/img/garden-grass-meadow-green.jpg">
+					<h5>Good Reputation Since'89</h5>
+					<p>Lorem ipsum dolor sit amet</p>
+					</div>
+
+					<div class="colom-2">
+					<img width="200" height="130" src="aset/img/pexels-photo-707194.jpeg">
+					<h5>Legal Company</h5>
+					<p>Lorem ipsum dolor sit amet</p>
+					</div>
+
+					<div class="colom-3">
+					<img width="200" height="130" src="aset/img/pexels-photo-169523.jpeg">
+					<h5>Focus On Quality</h5>
+					<p>Lorem ipsum dolor sit amet</p>
+					</div>
+
+					<div class="colom-4">
+					<img width="200" height="130" src="aset/img/pexels-photo-296230.jpeg">
+					<h5>Good Price</h5>
+					<p>Lorem ipsum dolor sit amet</p>
+					</div>
+
+				</div>
+			</div>
+
+		<div class="footer">
+			<div class="col">
+				<div class="col-ft-1">
+					<img src="aset/img/GG-w200.png">
+					<p>Sit omittam intellegam ad, vim no duis nulla prodesset, ad eos tale aliquid.Eam ne debet maiorum commune, noluisse gloriatur scripserit id vis</p>
+				</div>
+				<div class="col-ft-2">
+					<h2>Contact Us</h2>
+					<pre>
+					<i class="fa fa-phone">&nbsp&nbsp&nbsp</i>+6288985158738
+					<i class="fa fa-envelope">&nbsp&nbsp&nbsp</i>hey@greengarden.com
+					<i class="fa fa-map-marker">&nbsp&nbsp&nbsp</i> Karang Sari Cluwak Pati
+					<i class="fa fa-envelope">&nbsp&nbsp&nbsp</i>Opening Hours : Monday - Friday 09:00 - 00:00
+					</pre>
+				</div>
+				<div class="col-ft-3">
+					<h2>Mail Us</h2>
+					<input type="text" name="email" placeholder="email">
+					<textarea placeholder="Di Isi BGST(Bang Ganteng Senang Tadarus)"></textarea>
+					<input type="submit" name="send" value="Send">
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+</body>
+</html>
